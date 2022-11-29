1628e510a79006c20b7075a00dbe6e6f3480c8d0
Anon <opss59332@gmail.com>
2022-11-29 12:47:01 +0530

    First version ready

---
+++ pages/index.jsx
@@ -0,0 +1,50 @@
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+
+export default function Home() {
+  const [promptInput, setPromptInput] = useState();
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    const response = await fetch("/api/generateText", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ promptInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.headings}>
+        <h1>Clear your Viking Doubts</h1>
+        <p></p>
+      </div>
+      <textarea
+        value={promptInput}
+        className={styles.promptInput}
+        type="text"
+        placeholder="Type in your questions..."
+        onChange={(e) => {
+          setPromptInput(e.target.value);
+        }}
+      />
+      <button className={styles.generateBtn} onClick={callGenerateEndpoint}>
+       {isGenerating ? "Getting the facts..." : "Get the answer"}
+      </button>
+      <div className={styles.outputDiv}>
+        <h2>Output</h2>
+        <p>{apiOutput ? apiOutput : ""}</p>
+      </div>
+    </div>
+  );
+}
